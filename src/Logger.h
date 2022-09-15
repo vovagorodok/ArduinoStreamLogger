1afff011bc528a7ae4ea6f665e6c7e24a6835ba2
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2022-09-15 22:34:46 +0200

    Alligment

--- src/Logger.h
+++ src/Logger.h
@@ -18,17 +18,17 @@
 #define LOGLEVEL LogLevel::disabled
 #endif
 
-#define LOG_TRACE     LOG<LogLevel::trace>()
-#define LOG_DEBUG     LOG<LogLevel::debug>()
-#define LOG_INFO      LOG<LogLevel::info>()
-#define LOG_WARNING   LOG<LogLevel::warning>()
-#define LOG_ERROR     LOG<LogLevel::error>()
+#define LOG_TRACE   LOG<LogLevel::trace>()
+#define LOG_DEBUG   LOG<LogLevel::debug>()
+#define LOG_INFO    LOG<LogLevel::info>()
+#define LOG_WARNING LOG<LogLevel::warning>()
+#define LOG_ERROR   LOG<LogLevel::error>()
 
-#define LOG_NO_ENDL_TRACE     LOG_NO_ENDL<LogLevel::trace>()
-#define LOG_NO_ENDL_DEBUG     LOG_NO_ENDL<LogLevel::debug>()
-#define LOG_NO_ENDL_INFO      LOG_NO_ENDL<LogLevel::info>()
-#define LOG_NO_ENDL_WARNING   LOG_NO_ENDL<LogLevel::warning>()
-#define LOG_NO_ENDL_ERROR     LOG_NO_ENDL<LogLevel::error>()
+#define LOG_NO_ENDL_TRACE   LOG_NO_ENDL<LogLevel::trace>()
+#define LOG_NO_ENDL_DEBUG   LOG_NO_ENDL<LogLevel::debug>()
+#define LOG_NO_ENDL_INFO    LOG_NO_ENDL<LogLevel::info>()
+#define LOG_NO_ENDL_WARNING LOG_NO_ENDL<LogLevel::warning>()
+#define LOG_NO_ENDL_ERROR   LOG_NO_ENDL<LogLevel::error>()
 
 #define LOG_CALL_IF_TRACE(ex)   if constexpr (isLogged(LogLevel::trace))   ex
 #define LOG_CALL_IF_DEBUG(ex)   if constexpr (isLogged(LogLevel::debug))   ex
@@ -79,23 +79,23 @@ constexpr bool isLogged(LogLevel level) {
 
 template <LogLevel level>
 constexpr __attribute__((always_inline)) inline auto LOG() {
-	if constexpr (isLogged(level)) {
-		return LogEntry();
-	} else {
-		return NoLogEntry();
-	}
+    if constexpr (isLogged(level)) {
+        return LogEntry();
+    } else {
+        return NoLogEntry();
+    }
 };
 
 template <LogLevel level>
 constexpr __attribute__((always_inline)) inline auto LOG_NO_ENDL() {
-	if constexpr (isLogged(level)) {
-		return LogNoEndlEntry();
-	} else {
-		return NoLogEntry();
-	}
+    if constexpr (isLogged(level)) {
+        return LogNoEndlEntry();
+    } else {
+        return NoLogEntry();
+    }
 };
 
 template <typename T>
 [[maybe_unused]] constexpr NoLogEntry operator<<(const NoLogEntry noLogEntry, T value) {
-	return noLogEntry;
+    return noLogEntry;
 }
