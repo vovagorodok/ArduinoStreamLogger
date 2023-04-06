68d73d7898acda42ae7ea0c7be5e49e6131f3267
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2023-04-06 16:33:22 +0200

    Simpler show_colors

--- tools/show_colors.py
+++ tools/show_colors.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python3
+
 import curses
 
 
 def main(stdscr):
     curses.start_color()
     curses.use_default_colors()
-    for i in range(0, curses.COLORS):
+    for i in range(curses.COLORS):
         curses.init_pair(i + 1, i, -1)
-    for i in range(0, 255):
         stdscr.addstr(f"{i - 1} ", curses.color_pair(i))
     stdscr.getch()
 
